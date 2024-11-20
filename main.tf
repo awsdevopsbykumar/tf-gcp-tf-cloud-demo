ab556718201a647d19c6989515b0ed508e306d5d
awsdevopsbykumar <64312757+awsdevopsbykumar@users.noreply.github.com>
2024-11-20 21:05:02 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+# Configure the Google Cloud provider
+provider "google" {
+  credentials = var.GOOGLE_CREDENTIALS
+  project = "alert-autumn-442107-g2"
+  region  = "us-east1-b"
+}
+
+terraform { 
+  cloud { 
+    
+    organization = "awsdevopsbykumar" 
+
+    workspaces { 
+      name = "tf-gcp-tf-cloud-demo" 
+    } 
+  } 
+}
+
+variable "GOOGLE_CREDENTIALS" {
+ default = ""
+}
+
+variable "project" {
+ default = ""
+}
+
+resource "google_compute_instance" "default" {
+  name         = "example-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Include public IP
+    }
+  }
+}
